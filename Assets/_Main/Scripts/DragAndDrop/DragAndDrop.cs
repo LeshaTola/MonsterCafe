87404679d771c1bdd4d31a66df7399ce089b5fe4
LeshaTola <tola.lesha@mail.ru>
2024-01-20 22:01:55 +0300

    Add drag and drop functionality

---
+++ Assets/_Main/Scripts/DragAndDrop/DragAndDrop.cs
@@ -0,0 +1,39 @@
+using System;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+	public event Action OnDragStarted;
+	public event Action OnDragEnded;
+
+	private Vector3 offset;
+	private Camera cameraMain;
+
+	private void Awake()
+	{
+		cameraMain = Camera.main;
+	}
+
+	private void OnMouseDown()
+	{
+		OnDragStarted?.Invoke();
+		offset = transform.position - GetMousePosition();
+	}
+
+	private void OnMouseDrag()
+	{
+		transform.position = GetMousePosition() + offset;
+	}
+
+	private void OnMouseUp()
+	{
+		OnDragEnded?.Invoke();
+	}
+
+	private Vector3 GetMousePosition()
+	{
+		var mousePos = cameraMain.ScreenToWorldPoint(Input.mousePosition);
+		mousePos.z = 0;
+		return mousePos;
+	}
+}
